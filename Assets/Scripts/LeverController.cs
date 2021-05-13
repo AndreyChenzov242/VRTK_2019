6d469416c9190ac2825f1fc0aa521251f21511ff
AndreyChenzov242 <61480749+AndreyChenzov242@users.noreply.github.com>
2021-05-13 23:16:42 +0300

    init commmit

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,74 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEventHelper;
+    using VRTK.Controllables;
+
+    public class LeverController : MonoBehaviour
+    {
+        public GameObject wall;
+
+#pragma warning disable 0618
+        protected VRTK_Control_UnityEvents controlEvents;
+#pragma warning restore 0618
+        protected VRTK_BaseControllable controllableEvents;
+
+        protected virtual void OnEnable()
+        {
+#pragma warning disable 0618
+            if (GetComponent<VRTK_Control>() != null && GetComponent<VRTK_Control_UnityEvents>() == null)
+            {
+                controlEvents = gameObject.AddComponent<VRTK_Control_UnityEvents>();
+            }
+            controlEvents = GetComponent<VRTK_Control_UnityEvents>();
+#pragma warning restore 0618
+            controllableEvents = GetComponent<VRTK_BaseControllable>();
+
+            ManageListeners(true);
+        }
+
+        protected virtual void OnDisable()
+        {
+            ManageListeners(false);
+        }
+
+        protected virtual void ManageListeners(bool state)
+        {
+            if (state)
+            {
+                if (controlEvents != null)
+                {
+                    controlEvents.OnValueChanged.AddListener(HandleChange);
+                }
+                if (controllableEvents != null)
+                {
+                    controllableEvents.ValueChanged += ValueChanged;
+                }
+            }
+            else
+            {
+                if (controlEvents != null)
+                {
+                    controlEvents.OnValueChanged.RemoveListener(HandleChange);
+                }
+                if (controllableEvents != null)
+                {
+                    controllableEvents.ValueChanged -= ValueChanged;
+                }
+            }
+        }
+
+        protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+        {
+            if (e.value == 1)
+            {
+                wall.GetComponent<VerticalSlider>().StartLowerWall();
+            }
+        }
+
+        protected virtual void HandleChange(object sender, Control3DEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/VerticalSlider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalSlider : MonoBehaviour
+{
+    [SerializeField] private float _speed = 8f;
+    [SerializeField] private float _startPoint = 4.9f;
+    [SerializeField] private float _endPoint = -5f;
+
+    private bool _isLoweringWall;
+    private Transform _transformWall;
+
+    private void Start()
+    {
+        _transformWall = gameObject.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(_isLoweringWall & _transformWall.position.y >= _endPoint)
+        {
+            WallMove(Vector3.down);
+        }
+
+        if(!_isLoweringWall & _transformWall.position.y < _startPoint)
+        {
+            WallMove(Vector3.up);
+        }
+    }
+
+    void WallMove(Vector3 vector)
+    {
+        transform.Translate(vector * (_speed * Time.deltaTime));
+    }
+
+    public void StartLowerWall()
+    {
+        _isLoweringWall = true;
+    }
+
+    public void StartLiftWall()
+    {
+        _isLoweringWall = false;
+    }
+}
